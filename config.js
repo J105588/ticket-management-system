216ff26615747238295a44180b8c6d402af72b70
J105588 <jxjin2010@gmail.com>
2025-08-19 03:33:56 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
  * GAS Web AppのURLを設定してください
  */
 
-// ★下記のURLをあなたのGAS Web App URLに置き換えてください
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyfN0c-ThK4kNnoiwbQkF4LfSK_LxlhmmMVpuAeAzoIR_CvNTem-aY-L4c3wY7lF3cJtw/exec";
+// GAS Web App URLに置き換え
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyU7gg1PCcwfYYFAvb2m5smTfWvcomvD2sfqn1Fdf3AbaHUsKJnL9LtOVZqn8VxHSdu9w/exec";
 
 // デバッグモード（開発時はtrue、本番はfalse）
 const DEBUG_MODE = true;
