4f52ed1b635e0ba688d6031459fb5774f42270c0
J105588 <jxjin2010@gmail.com>
2025-08-19 14:14:56 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  */
 
 // GAS Web App URLに置き換え
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbwjTtREFD5tpTUzkId0ykeW7oaGFspq-kQ_Ng62mg-Fl-OIayCTOpHNKlqsQMRRV1Y/exec";
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbwejZIDuzJIIdVkdz2nbYY8iwuTQW-J8I24khdUwSYSdXSnv3WFC0tlYT98fo7EeQs9/exec";
 
 // デバッグモード（開発時はtrue、本番はfalse）
 const DEBUG_MODE = true;
